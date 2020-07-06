4358b613841dc8e7e3a9069ddbae9ed1bfa98786
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-06 21:10:00 +0530

    Added fourth computation a%b+c

--- Computation.sh
+++ Computation.sh
@@ -8,7 +8,7 @@ echo "Enter value of c:"
 read  c
 
 Compute1=`echo $b $c $a | awk '{print '$b' * '$c' + '$a' }'`
-echo "a + b *c = $Compute1"
+echo "a + b * c = $Compute1"
 
 Compute2=`echo $a $b $c | awk '{print '$a' * '$b' + '$c' }'`
 echo "a * b + c = $Compute2"
@@ -16,4 +16,5 @@ echo "a * b + c = $Compute2"
 Compute3=`echo $a $b $c | awk '{print '$c' + '$a' / '$b' }'`
 echo "c + a / b = $Compute3"
 
-
+Compute4=`echo $a $b $c | awk '{print '$a' % '$b' + '$c' }'`
+echo "a % b + c = $Compute4"
